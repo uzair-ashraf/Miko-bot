9de9cff5b342eea2760ca9dffd5f72b908c32538
Uzair Ashraf <uzinatorcl@live.com>
2019-12-18 19:54:53 -0800

    added help message, will update along other branches'

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const SlackBot = require('slackbots');
+const { help } = require('./help');
 
 const bot = new SlackBot({
   token: process.env.SLACK_TOKEN,
   name: "mikobot"
 });
 
+const commands = {
+  help
+}
+console.log(commands);
+
 bot.on('message', data => {
   if(data.type !== 'message') return;
   const message = data.text.split(" ");
   if(message[0] !== 'miko') return;
-    // bot.postMessage(data.user, 'no u');
-    // bot.postMessageToUser(data.user, message);
-  // bot.openIm(data.user)
-  // .then(data => console.log(data))
-  console.log(data.channel);
-  bot.postMessage(data.channel, 'Hello!')
+  if(message[1] === undefined) {
+    bot.postMessage(data.channel, `
+  Hello~~ \n For a list of my commands please type "miko help" 0W0.
+  `)
+  return;
+  }
+  if(commands.hasOwnProperty(message[1])) {
+    commands[message[1]](bot, data);
+  }
 })
