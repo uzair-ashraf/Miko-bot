052a05d3eb1bb47558ab3f26716c0ea870977511
Uzair Ashraf <uzinatorcl@live.com>
2019-12-19 19:05:36 -0800

    modded question slightly, looking for ways to add typing message from bot

--- question.js
+++ question.js
@@ -1,4 +1,7 @@
 const question = (bot, data) => {
+
+  bot.sendTyping(data.channel);
+
   bot.postMessage(data.channel, `
   So you wan't to know how to ask an instructor a question I see.
   `)
@@ -19,10 +22,15 @@ const question = (bot, data) => {
         bot.postMessage(data.channel, `
         @LFZ-Teacher please help, I'm lost
         `)
-      },1400))
-    }, 1400))
-  }, 1400))
-  }, 1400))
+          .then( () => setTimeout( () => {
+            bot.postMessage(data.channel, `
+            As you can see questions like that can make things difficult for us to interpret.
+            `)
+          }, 2200))
+      },2200))
+    }, 2200))
+  }, 2200))
+  }, 2200))
 }
 
 module.exports = {
