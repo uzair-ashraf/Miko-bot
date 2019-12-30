663127e4f8202b2f04192ca4b8cb6b78ad9f4097
Uzair Ashraf <uzinatorcl@live.com>
2019-12-29 22:32:48 -0800

    basic setup for google api is there buuuut, kaetai

--- help.js
+++ help.js
@@ -1,5 +1,7 @@
-const help = (bot, data) => {
-  bot.postMessage(data.channel, `
+const miko = require('./miko');
+
+const help = (data) => {
+  miko.postMessage(data.channel, `
   Please remember I don't hear very well and I need you to say miko before any of my commands.
  help: List of commands
  question: How to ask an instructor a question
--- scheduleSession.js
+++ scheduleSession.js
@@ -1,15 +1,54 @@
 const miko = require('./miko');
+const mikoCreds = require('./miko-creds');
+const { google } = require('googleapis');
 
+const SCOPES = 'https://www.googleapis.com/auth/calendar.events';
+const { client_email: GOOGLE_CLIENT_EMAIL,
+        private_key: GOOGLE_PRIVATE_KEY
+      } = mikoCreds;
+
+const jwtClient = new google.auth.JWT(
+  GOOGLE_CLIENT_EMAIL,
+  null,
+  GOOGLE_PRIVATE_KEY,
+  SCOPES
+)
+
+const { GOOGLE_PROJECT_NUMBER } = process.env;
+const calendar = google.calendar({
+  version: 'v3',
+  project: GOOGLE_PROJECT_NUMBER,
+  auth: jwtClient
+})
 class ScheduleSession {
-  constructor() {
+  constructor(calendarApiClient, calendarId) {
     this.sessions = {}
+    this.calendarApi = calendarApiClient
+    this.calendarId = calendarId
+    this.currentWeeksSchedule = null;
   }
   startSession(data) {
     const { user, channel } = data;
     this.sessions[user] = {}
     this.sessions[user].channel = channel;
     this.handleTimeUpdate(user)
+    this.getCurrentSchedule()
     console.log("session starting")
+  }
+  getCurrentSchedule() {
+    this.calendarApi.events.list({
+      calendarId: this.calendarId,
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime'
+    },(err, res) => {
+      if(err) {
+        console.log(err)
+      } else {
+        console.log(res)
+      }
+    })
   }
   isInSession(userId) {
     return !!this.sessions[userId]
@@ -29,6 +68,6 @@ class ScheduleSession {
   }
 }
 
-const sessions = new ScheduleSession();
+const sessions = new ScheduleSession(calendar, process.env.CALENDAR_ID);
 
 module.exports = sessions;
